4219080398293fe002aa2173fd6dadbebdfe0cb2
aryanPatel <aryanpatel1248@gmail.com>
2024-02-25 11:24:05 +0530

    learn about how to write in file system

--- nodeLearning/readwrite.js
+++ nodeLearning/readwrite.js
@@ -1,3 +1,5 @@
+//***read the content of file */
+
 //step1: creat object
 // read in manner of syncronus
 // const fs = require('fs');
@@ -8,12 +10,32 @@
 
 
 //read in manner of asyncronus
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.readFile('input.txt',(err,content)=>{
+// fs.readFile('input.txt',(err,content)=>{
+//     if(err){
+//         return console.log(err)
+//     }
+//     console.log("content of file is:"+content)
+// })
+// console.log("last line")
+
+
+////** write the file */
+
+//syncronus way
+// const fs = require('fs');
+// console.log("before");
+// fs.writeFileSync("output.txt","hello everyone")
+// console.log("after")
+
+//asyncronus way
+console.log("first line");
+const fs = require('fs');
+fs.writeFile('output2.txt',"hello student",err=>{
     if(err){
-        return console.log(err)
+        return console.log(err);
     }
-    console.log("content of file is:"+content)
+    console.log("written success fully")
 })
 console.log("last line")
