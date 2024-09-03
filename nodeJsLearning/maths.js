f2f4cdd537921e9ba393ecf1f6a8fd377c246ede
aryanPatel <aryanpatel1248@gmail.com>
2024-09-03 11:24:11 +0530

    first commit in backend

---
+++ nodeJsLearning/maths.js
@@ -0,0 +1,6 @@
+module.exports.add = (a,b)=>a+b;
+module.exports.mul =(a,b)=>a*b;
+module.exports.pi = 3.14;
+
+ 
+ 
---
+++ nodeJsLearning/script.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+const data = require("./maths")
+
+console.log(data.add(10,10));
+console.log(data.mul(10,5));
+console.log(data.pi);
